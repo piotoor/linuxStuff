7f4bcffdc7bf0b19e6a44447b91ae6e448eb80a2
Piotr Kozka <piotoor@gmailcom>
2016-09-08 00:43:10 +0200

    Created simple startup script for commonl used application installation

---
+++ startup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#sudo apt-get update;
+
+echo "-------------------- Downloading music related apps -------------------- ";
+sudo apt-get --assume-yes install banshee;
+
+echo "-------------------- Downloading video related apps -------------------- ";
+sudo apt-get --assume-yes install smplayer;
+sudo apt-get --assume-yes install qnapi;
+
+echo "-------------------- Downloading image browsers and editors -------------------- ";
+sudo apt-get --assume-yes install gthumb;
+
+echo "-------------------- Downloading development stuff -------------------- ";
+sudo apt-get --assume-yes install git;
+sudo apt-get --assume-yes install vim;
+sudo apt-get --assume-yes install tree;
+
